beda10829131f78394524e8b21027840533d1297
Elvin cooper <ing.elvin01cooper@gmail.com>
2025-07-10 00:26:29 -0400

    probando la app para despliegue

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key = 'ecooper123456789'
+    
     from . import urlshort
     app.register_blueprint(urlshort.bp)
+    
     return app
 
